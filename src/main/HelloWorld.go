b1158989cf78a929ac85cfc69d3b39e198cd5aeb
hailyboy <31983644+hailyboy@users.noreply.github.com>
2018-09-08 22:38:57 +0800

    first commit

---
+++ src/main/HelloWorld.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"mylog"
+)
+
+func main(){
+	fmt.Print("Hello go")
+
+	mylog.GenLog("你好！")
+}
---
+++ src/mylog/genLog.go
@@ -0,0 +1,7 @@
+package mylog
+
+import "fmt"
+
+func GenLog( str string){
+	fmt.Print("infor :" +str)
+}
